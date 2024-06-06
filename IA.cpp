c98b7df8098ca7018f531ee2848a6fdf66121c07
Amélie <amelie.nader.prieto@alumnos.upm.es>
2024-06-06 16:05:36 +0200

    Bucle en la función mover_rndm
    Ahora, en caso de elegir una pieza sin jugadas legales, elegirá una distinta hasta que pueda mover

--- IA.cpp
+++ IA.cpp
@@ -9,14 +9,34 @@ void IA::inicializar(Tablero& tab) {
 }
 
 void IA::mover_rndm(Tablero& tab) {
-	for (auto &i : tab.get_piezas_neg())
+	cout << "\nTURNO DE LA MÁQUINA\n";
+	piezas_propias.clear();
+	for (auto& i : tab.get_piezas_neg())
 		piezas_propias.push_back(i);
-	Pieza pieza = piezas_propias[GetRandom(piezas_propias.size())];
-	vector<Vector2D>posibles_mov;
-	if (puede_mover(pieza, posibles_mov, tab)) {
-		mover_pieza(pieza.GetPosicion(), posibles_mov[GetRandom(posibles_mov.size())], tab);
-		fin_turno = true;
+	fin_turno = false;
+	while (!fin_turno) {
+		// Elige una peiza al azar 
+		Pieza pieza = piezas_propias[GetRandom(piezas_propias.size())];
+
+		cout << " " << pieza << " ";
+
+		vector<Vector2D>posibles_mov = obtener_movimientos_legales(pieza.GetPosicion(), tab);
+		//if (puede_mover(pieza, posibles_mov, tab)) {
+		//	// si puede, mueve la pieza a una casilla al azar
+		//	mover_pieza(pieza.GetPosicion(), posibles_mov[GetRandom(posibles_mov.size())], tab);
+		//	fin_turno = true;
+		//}
+		//else cout << "  esa pieza no se puede mover crack xdxd  ";
+		if (posibles_mov.size() == 0) {
+			cout << " esa pieza no se puede mover crack xdxd ";
+		}
+		else {
+			auto casilla_destino = posibles_mov[GetRandom(posibles_mov.size())];
+			mover_pieza(pieza.GetPosicion(), casilla_destino, tab);
+			fin_turno = true;
+		}
 	}
+	piezas_propias.clear();
 }
 
 bool IA::puede_mover(Pieza& pieza, vector<Vector2D> &posibles_mov, Tablero& tab)
